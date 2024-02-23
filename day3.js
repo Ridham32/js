de4f1e105a84ef8da04b214ece345bcb9a67fbee
Ridham32 <sharmaridham473@gmail.com>
2024-02-23 10:26:01 +0530

    ...

--- day3.js
+++ day3.js
@@ -64,15 +64,49 @@ var studentList = [
 // }
 
 
-let upcomingBatch = studentList.filter((element)=>{
-    return element.upcoming == "Node Js"
-})
+// let upcomingBatch = studentList.filter((element)=>{
+//     return element.upcoming == "Node Js"
+// })
 
-let colleges = studentList.filter((item)=>{
-     if( item.college == "Daviet"){
-        console.log(item.name);
-     }
-})
+// let colleges = studentList.filter((item)=>{
+//      if( item.college == "Daviet"){
+//         console.log(item.name);
+//      }
+// })
+
+// var studentObj = {
+//     roll: 3,
+//     name: "Surbhi",
+//     currentCourse: "Advance Js",
+//     upcoming: "Angular",
+//     college: "Cjs",
+//     "company course": "Flutter + Nodejs",
+//     institute: {
+//       seniorSecondary: "ABC school",
+//       graduation: "Cjs",
+//       postgraduate: "Nit jal",
+//     },
+// }
+
+// console.log("---------------------------------");
+
+// studentList.push(studentObj)
+// for(const i of studentList){
+//     console.log(i.name);
+// }
+// console.log("---------------------------------");
+// console.log(studentList[3]);
+
+var studentDetail = JSON.stringify(studentList)
+console.log(studentDetail);
+console.log("---------------------------------");
+
+
+var parsedstudentDetail = JSON.parse(studentList)
+console.log(typeof parsedstudentDetail);
+
+
+
 
 
 
